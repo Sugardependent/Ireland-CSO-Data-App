6cc744588f8664dcbb3ad92a7a0b445ddcc1e064
sugardependent <inver7777@hotmail.com>
2017-10-01 02:41:33 +0100

    Cleaned up folders

--- angular-app/src/app/graphdata.ts
+++
@@ -1,7 +0,0 @@
-export interface GraphData {
-  region_name: string;
-  graph_name: string;
-  gender: string;
-  data_id: number;
-  points: any[];
-}
--- backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ronan/Code/web_dev/cso_app/Ireland-CSO-Data-App/backend/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
